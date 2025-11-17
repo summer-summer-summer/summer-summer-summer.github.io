6c3bd8efa0102f672aaef4906e52f81f88d6972c
Changchangchang <1503047402@qq.com>
2025-11-17 15:09:06 +0800

    updated

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,7 @@
+
+export default new Map([
+["src/content/post/haptic-wings/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fhaptic-wings%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/post/dynamic-city/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fdynamic-city%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/post/beauty-eyes/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fbeauty-eyes%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/post/petbot/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fpost%2Fpetbot%2Findex.mdx&astroContentModuleFlag=true")]]);
+		
